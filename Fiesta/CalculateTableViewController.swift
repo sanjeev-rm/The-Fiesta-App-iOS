1a46d18916b211c01c97054992f164b44edd7d01
sanjeev-rm <Sanjeevraghu2050@gmail.com>
2022-12-31 21:36:58 +0530

    Added / Edited comments.

--- Fiesta/CalculateTableViewController.swift
+++ Fiesta/CalculateTableViewController.swift
@@ -22,6 +22,9 @@ class CalculateTableViewController: UITableViewController
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
+    /// This function calculates the amount to be paid by a person.
+    /// - parameters: person -> The person of type Person.
+    /// - parameters: tax -> the tax percentage.
     func calculateAmountToBePaidByPerson(person : Person, tax : Double) -> Double
     {
         guard let itemsList = person.itemsHad else { return 0 }
@@ -65,50 +68,4 @@ class CalculateTableViewController: UITableViewController
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
