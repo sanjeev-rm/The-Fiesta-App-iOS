ea002b7899ef6cc9d200a3d8f8693b722fb79725
sanjeev-rm <Sanjeevraghu2050@gmail.com>
2023-01-06 22:38:46 +0530

    Updated the UI.

---
+++ Fiesta/BeginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  BeginViewController.swift
+//  Fiesta
+//
+//  Created by Sanjeev RM on 06/01/23.
+//
+
+import UIKit
+
+class BeginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func beginButtonTapped(_ sender: UIButton)
+    {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
